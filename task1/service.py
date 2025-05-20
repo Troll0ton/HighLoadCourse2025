941e2f653e23855294ab95b0c4019536dba77234
Troll0ton <111769737+Troll0ton@users.noreply.github.com>
2025-05-20 22:13:41 +0300

    fix

--- task1/service.py
+++ task1/service.py
@@ -1,27 +1,29 @@
-from flask import Flask, jsonify, request
 from datetime import datetime
-import argparse
+
+from flask import Flask, jsonify, request
 
 service = Flask(__name__)
 
 RECORD_NUMBER = 10000
 
+
 @service.route('/date_service', methods=['GET'])
 def date_service():
     now = datetime.now()
-    
+
     records = []
     for _ in range(RECORD_NUMBER):
         records.append({"year": now.year, "month": now.month, "day": now.day})
-    
+
     return jsonify(records)
+
 
 @service.route('/name_service', methods=['POST'])
 def name_service():
-    name = request.form.get('name') 
-    
+    name = request.form.get('name')
+
     records = []
     for _ in range(RECORD_NUMBER):
         records.append({"name": name})
-        
+
     return jsonify(records)
